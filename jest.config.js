f90818376dfb9e2f5e9e0ee55f2aa0101d617332
Alex Hatzopoulos <prometheusgr@Live.com>
2026-05-14 07:27:08 -0700

    style: normalize importer config formatting

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 /** @type {import('jest').Config} */
 const config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: 'src',
-  testMatch: ['**/__tests__/**/*.ts', '**/*.test.ts', '**/*.spec.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: "src",
+  testMatch: ["**/__tests__/**/*.ts", "**/*.test.ts", "**/*.spec.ts"],
   collectCoverageFrom: [
-    '**/*.ts',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-    '!**/__tests__/**',
+    "**/*.ts",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/__tests__/**",
   ],
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  moduleFileExtensions: ["ts", "js", "json"],
 };
 
 module.exports = config;
